d5963bb6e370d76595c6aa84812552e7995f349f
cloud666666666 <2353493891@qq.com>
2026-08-15 21:03:10 +0800

    rebuild blog with Astro + astro-paper

---
+++ astro-paper.config.ts
@@ -0,0 +1,42 @@
+import { defineAstroPaperConfig } from "./src/types/config";
+
+export default defineAstroPaperConfig({
+  site: {
+    url: "https://cloud666666666.github.io/",
+    title: "Yunhao Wang",
+    description: "Yunhao Wang's tech blog: embodied AI, VLA, computer vision and hands-on engineering.",
+    author: "Yunhao Wang",
+    profile: "https://github.com/cloud666666666",
+    ogImage: "default-og.jpg",
+    lang: "zh-CN",
+    timezone: "Asia/Shanghai",
+    dir: "ltr",
+  },
+  posts: {
+    perPage: 6,
+    perIndex: 6,
+    scheduledPostMargin: 15 * 60 * 1000,
+  },
+  features: {
+    lightAndDarkMode: true,
+    dynamicOgImage: false,
+    showArchives: true,
+    showBackButton: true,
+    editPost: {
+      enabled: true,
+      url: "https://github.com/cloud666666666/cloud666666666.github.io/edit/main/",
+    },
+    search: "pagefind",
+  },
+  socials: [
+    { name: "github",   url: "https://github.com/cloud666666666" },
+    { name: "mail",     url: "mailto:2353493891@qq.com" },
+  ],
+  shareLinks: [
+    { name: "whatsapp", url: "https://wa.me/?text=" },
+    { name: "facebook", url: "https://www.facebook.com/sharer.php?u=" },
+    { name: "x",        url: "https://x.com/intent/post?url=" },
+    { name: "telegram", url: "https://t.me/share/url?url=" },
+    { name: "mail",     url: "mailto:?subject=See%20this%20post&body=" },
+  ],
+});
